d7795da18f2d860fe3f581d3567e325f1a31a41d
SergeJohanns <serge.johanns@gmail.com>
2020-01-13 19:12:15 +0100

    Added terminal control

---
+++ FCores/terminus.py
@@ -0,0 +1,21 @@
+from threading import Timer
+from FunctionalityCore import FCore
+
+PROMPT = "$ "
+
+class terminus(FCore):
+    """Take control of the terminal after core loading and allow the user to defer it to the other cores."""
+
+    def __init__(self, bot):
+        super().__init__(bot)
+        terminus = Timer(0.1, self.terminal_control)
+        terminus.daemon = True
+        terminus.start()
+
+    def terminal_control(self):
+        print("\nTERMINUS CONTROL CORE")
+        while True:
+            try:
+                self.bot.cores[input(PROMPT)].terminal_control()
+            except KeyError:
+                print("No such core is loaded.")
--- FunctionalityCore.py
+++ FunctionalityCore.py
@@ -7,6 +7,10 @@ class FCore:
     def get_commands(self) -> dict:
         """Return a dictionary of all commands and their corresponding callbacks."""
         return dict()
+
+    def terminal_control(self):
+        """Provide in-terminal functionality after being given control."""
+        print("This core does not have terminal functionality.")
     
     def not_command(self, update, context):
         """Called if the bot recieves an unrecognised command."""

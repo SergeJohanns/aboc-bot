2b811761d37bb62d98ae45f6df946c6e2c3d9231
SergeJohanns <serge.johanns@gmail.com>
2020-01-25 01:22:54 +0100

    Added support for private ballots

--- FCores/democracy.py
+++ FCores/democracy.py
@@ -1,6 +1,8 @@
 import time
+import traceback
+import functools
 from FunctionalityCore import FCore
-from kerberos import require_ring
+from FCores.kerberos import require_ring
 
 class democracy(FCore):
     """Allow for arbitrary polling in group chats."""
@@ -11,15 +13,16 @@ class democracy(FCore):
         self.polls = dict()
     
     def get_commands(self) -> dict:
-        return {"poll":self.poll, "yay":self.yay, "nay":self.nay, "closepoll":self.closepoll}
+        return {"poll":self.poll, "yay":functools.partial(self.vote, "yay"), "nay":functools.partial(self.vote, "nay"), "closepoll":self.closepoll}
     
     @require_ring(2)
     def poll(self, update, context):
         if update.effective_chat.id in self.polls:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="This chat already has a poll running: " + self.polls[update.effective_chat.id])
-        poll = update.effective_message.text.split(' ', 1)[1]
-        self.polls[update.effective_chat.id] = {"poll":poll, "voters":{}, "yay":0, "nay":0}
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"New poll: {poll} Vote in favour or against with /yay or /nay respectively.")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="This chat already has a poll running: " + self.polls[update.effective_chat.id]["poll"])
+        else:
+            poll = update.effective_message.text.split(' ', 1)[1]
+            self.polls[update.effective_chat.id] = {"poll":poll, "voters":set(), "yay":0, "nay":0}
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"New poll: {poll} Vote in favour or against with /yay or /nay respectively.")
         
     def closepoll(self, update, context):
         if not update.effective_chat.id in self.polls:
@@ -30,18 +33,18 @@ class democracy(FCore):
         context.bot.send_message(chat_id=update.effective_chat.id, text=f"For the yays, {result['yay']}. For the nays, {result['nay']}.")
         del self.polls[update.effective_chat.id]
 
-    def yay(self, update, context):
-        if update.effective_user.id not in self.polls[update.effective_chat.id]["voters"]:
-            self.polls[update.effective_chat.id]["yay"] += 1
-            self.polls[update.effective_chat.id]["voters"].add(update.effective_user.id)
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Registered yay vote.", reply_to_message_id=update.effective_message.message_id)
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You have already voted in this poll.", reply_to_message_id=update.effective_message.message_id)
-
-    def nay(self, update, context):
-        if update.effective_user.id not in self.polls[update.effective_chat.id]["voters"]:
-            self.polls[update.effective_chat.id]["nay"] += 1
-            self.polls[update.effective_chat.id]["voters"].add(update.effective_user.id)
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Registered nay vote.", reply_to_message_id=update.effective_message.message_id)
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="You have already voted in this poll.", reply_to_message_id=update.effective_message.message_id)
+    def vote(self, result:str, update, context):
+        if update.effective_chat.id in self.polls:
+            if update.effective_user.id not in self.polls[update.effective_chat.id]["voters"]:
+                self.polls[update.effective_chat.id]["voters"].add(update.effective_user.id)
+                self.polls[update.effective_chat.id][result] += 1
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Registered " + result + " vote.", reply_to_message_id=update.effective_message.message_id)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="You have already voted in this poll.", reply_to_message_id=update.effective_message.message_id)
+        elif (chat := self.bot.cores["prism"].by_chat_title(update.effective_message.text.split(' ', 1)[1]))["id"] in self.polls:
+            if update.effective_user.id not in self.polls[chat["id"]]["voters"]:
+                self.polls[chat["id"]]["voters"].add(update.effective_user.id)
+                self.polls[chat["id"]][result] += 1
+                context.bot.send_message(chat_id=update.effective_chat.id, text="Registered " + result + " vote.", reply_to_message_id=update.effective_message.message_id)
+            else:
+                context.bot.send_message(chat_id=update.effective_chat.id, text="You have already voted in this poll.", reply_to_message_id=update.effective_message.message_id)
